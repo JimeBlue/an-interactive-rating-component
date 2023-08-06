5c012a4733cb9f175ac0458dd0eee2a3049c9431
Jimena Miguez <j.miguez@airlst.com>
2023-08-06 15:22:05 +0200

    Create Rating component with template and data definition

---
+++ components/Rating.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-if="!submitted">
+      <h2>Rate us:</h2>
+      <div class="stars">
+        <label
+          v-for="(star, index) in stars"
+          :key="index"
+          :for="'star_' + index"
+        >
+          <input
+            type="radio"
+            :id="'star_' + index"
+            :value="index + 1"
+            v-model="selectedRating"
+            class="star-input"
+          />
+          {{ star }}
+        </label>
+      </div>
+      <button>Submit Rating</button>
+    </div>
+    <div v-else>
+      <h2>Thank you for your rating!</h2>
+      <p>You rated us: {{ submittedRating }} stars</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stars: ["★", "★", "★", "★", "★"],
+      highlighted: -1,
+      submitted: false,
+      submittedRating: 0,
+      selectedRating: null,
+    };
+  },
+};
+</script>
